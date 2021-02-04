17523556fdf94dd60c2da8ae977b77d58ba5371e
Bertrand Bourion <bertrandbou@hotmail.fr>
2021-02-04 18:15:42 +0100

    feat(user-controller): edit current user's profile

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -9,12 +9,15 @@ function getUserRoutes() {
   const router = express.Router();
 
   router.get('/', protect, getRecommendedChannels);
+  router.put('/', protect, editUser);
+
   router.get('/liked-videos', protect, getLikedVideos);
   router.get('/history', protect, getHistory);
-  router.get('/:userId', getAuthUser, getProfile);
-  router.get('/:userId/toggle-subscribe', protect, toggleSubscribe);
   router.get('/subscriptions', protect, getFeed);
   router.get('/search', getAuthUser, searchUser);
+
+  router.get('/:userId', getAuthUser, getProfile);
+  router.get('/:userId/toggle-subscribe', protect, toggleSubscribe);
 
   return router;
 }
@@ -362,6 +365,22 @@ async function getProfile(req, res, next) {
   res.status(200).json({ user });
 }
 
-async function editUser(req, res) {}
+async function editUser(req, res) {
+  const { username, cover, avatar, about } = req.body;
+
+  const user = await prisma.user.update({
+    where: {
+      id: req.user.id,
+    },
+    data: {
+      username,
+      cover,
+      avatar,
+      about,
+    },
+  });
+
+  res.status(200).json({ user });
+}
 
 export { getUserRoutes };
