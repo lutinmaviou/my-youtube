6e830d194b1a6fb9c3a1e3b43514d2c7f70f8994
Bertrand Bourion <bertrandbou@hotmail.fr>
2021-02-10 16:23:30 +0100

    feat(authentication): add sign in with Google OAuth

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,7 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { protect } from '../middleware/authorization';
+import { OAuth2Client } from 'google-auth-library';
 
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const prisma = new PrismaClient();
 
 // A function to get the routes.
@@ -20,7 +22,13 @@ function getAuthRoutes() {
 
 // All controllers/utility functions here
 async function googleLogin(req, res) {
-  const { username, email } = req.body;
+  const { idToken } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  const { name, picture, email } = ticket.getPayload();
+
   let user = await prisma.user.findUnique({
     where: {
       email,
@@ -30,8 +38,9 @@ async function googleLogin(req, res) {
   if (!user) {
     user = await prisma.user.create({
       data: {
-        username,
         email,
+        username: name,
+        avatar: picture,
       },
     });
   }
