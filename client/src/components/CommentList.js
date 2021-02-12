1f6d55cebe4430375cd6384bb0f85af4bb89aca5
Bertrand Bourion <bertrandbou@hotmail.fr>
2021-02-12 16:52:38 +0100

    refactor(commentList component): add memoization to not rerender when writing a new comment

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { formatCreatedAt } from 'utils/date';
 import DeleteCommentDropdown from './DeleteCommentDropdown';
 
 function CommentList({ comments }) {
-  return comments.map((comment) => (
-    <Comment key={comment.id} comment={comment} />
-  ));
+  return comments.map((comment) => <Comment key={comment.id} comment={comment} />);
 }
 
 function Comment({ comment }) {
   return (
     <div className="comment">
       <Link to={`/channel/${comment.user.id}`}>
-        <img
-          src={comment.user.avatar}
-          alt={`${comment.user.username} avatar`}
-        />
+        <img src={comment.user.avatar} alt={`${comment.user.username} avatar`} />
       </Link>
       <div className="comment-info" style={{ flex: '1 1 0' }}>
         <p className="secondary">
@@ -36,4 +31,4 @@ function Comment({ comment }) {
   );
 }
 
-export default CommentList;
+export default memo(CommentList);
